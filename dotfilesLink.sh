c2b53540c2449bca74dcffb83441b4274b5bf94d
Ryoya Urano <uryoya.ur@gmail.com>
2016-03-01 02:47:44 +0900

    血迷ってたところを戻した

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,11 +1,11 @@
 #! /bin/sh
 
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/.gvimrc ~/.gvimrc
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/.vim ~/.vim
 mkdir ~/dotfiles/.vim/tmp
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
 sh installer.sh ~/.vim
 rm -rf installer.sh
 
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.gvimrc ~/.gvimrc
-ln -sf ~/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/.vim ~/.vim
