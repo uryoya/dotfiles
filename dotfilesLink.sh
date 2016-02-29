f6c4249c0b4d2fe3521ff49dd4d19df18994f849
Ryoya Urano <uryoya.ur@gmail.com>
2016-03-01 02:32:29 +0900

    deinを勝手にインストールするようにした

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,4 +1,10 @@
 #! /bin/sh
+
+mkdir ~/.vim/tmp
+curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
+sh installer.sh ~/.vim
+rm -rf installer.sh
+
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.gvimrc ~/.gvimrc
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
