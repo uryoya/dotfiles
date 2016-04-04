58b4e39ae5d50b8f3845faf202105937df27c642
RyoyaUrano <urano.works.mail@gmail.com>
2016-04-04 09:39:15 +0900

    nvimとxresourcesを追加

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -4,4 +4,5 @@ ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.gvimrc ~/.gvimrc
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.vim ~/.vim
-
+ln -sf ~/dotfiles/nvim ~/.config/nvim
+ln -sf ~/dotfiles/.Xresources ~/.Xresources
