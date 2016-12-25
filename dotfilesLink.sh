a980a566f02a8065f75af13a1f88fae3a6517751
URANO Ryoya <uryoya.ur@gmail.com>
2016-12-25 22:22:17 +0900

    .tmux.conf追加

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,9 +1,10 @@
 #! /bin/sh
 
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.gvimrc ~/.gvimrc
-ln -sf ~/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/.vim ~/.vim
-ln -sf ~/dotfiles/nvim ~/.config/nvim
-ln -sf ~/dotfiles/.Xresources ~/.Xresources
-ln -sf ~/dotfiles/.zshenv ~/.zshenv
+ln -sf $HOME/dotfiles/.vimrc $HOME/.vimrc
+ln -sf $HOME/dotfiles/.gvimrc $HOME/.gvimrc
+ln -sf $HOME/dotfiles/.zshrc $HOME/.zshrc
+ln -sf $HOME/dotfiles/.vim $HOME/.vim
+ln -sf $HOME/dotfiles/nvim $HOME/.config/nvim
+ln -sf $HOME/dotfiles/.Xresources $HOME/.Xresources
+ln -sf $HOME/dotfiles/.zshenv $HOME/.zshenv
+ln -sf $HOME/dotfiles/.tmux.conf $HOME/.tmux.conf
