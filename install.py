3e8bfb278a11daaef4c8ff6a7b108059d9ebf023
URANO Ryoya <uryoya.ur@gmail.com>
2016-12-27 03:21:24 +0900

    :new: add install.py

---
+++ install.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import os
+from pathlib import Path
+
+
+DOTFILES_DIR = Path.home() / 'dotfiles'
+
+dotfiles = [
+    [DOTFILES_DIR / '.Xresources', Path.home() / '.Xresources'],
+    [DOTFILES_DIR / '.vimrc', Path.home() / '.vimrc'],
+    [DOTFILES_DIR / '.gvimrc', Path.home() / '.gvimrc'],
+    [DOTFILES_DIR / '.vim', Path.home() / '.vim'],
+    [DOTFILES_DIR / 'nvim', Path.home() / '.config/nvim'],
+    [DOTFILES_DIR / '.zshrc', Path.home() / '.zshrc'],
+    [DOTFILES_DIR / '.zshenv', Path.home() / '.zshenv'],
+    [DOTFILES_DIR / '.bashrc', Path.home() / '.bashrc'],
+    [DOTFILES_DIR / '.tmux.conf', Path.home() / '.tmux.conf'],
+]
+
+for src, dst in dotfiles:
+    try:
+        os.symlink(src, dst)
+    except OSError as e:
+        print(e)
