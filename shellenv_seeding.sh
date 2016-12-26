0d68d310b63808c4306fca74b59273a7fa75f174
URANO Ryoya <uryoya.ur@gmail.com>
2016-12-26 19:01:49 +0900

    shebang変更

--- shellenv_seeding.sh
+++ shellenv_seeding.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#! /bin/bash
 
 ln -sf $HOME/dotfiles/.vimrc $HOME/.vimrc
 ln -sf $HOME/dotfiles/.gvimrc $HOME/.gvimrc
@@ -8,3 +8,5 @@ ln -sf $HOME/dotfiles/nvim $HOME/.config/nvim
 ln -sf $HOME/dotfiles/.Xresources $HOME/.Xresources
 ln -sf $HOME/dotfiles/.zshenv $HOME/.zshenv
 ln -sf $HOME/dotfiles/.tmux.conf $HOME/.tmux.conf
+
+
