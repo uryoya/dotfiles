48a6e3a26d30a5c8ab39fe61454ee4285c0231b4
URANO Ryoya <uryoya.ur@gmail.com>
2017-02-21 02:36:48 +0900

    インストール処理の追加

--- install.sh
+++ install.sh
@@ -1,5 +1,16 @@
 #! /bin/bash
 
+# ツールのインストール
+sudo apt update && sudo apt upgrade -y
+sudo apt install zsh tmux vim-gnome
+# for neovim
+sudo apt install rsync xsel python-pip python3-pip
+sudo pip install --upgrade pip && sudo pip3 install --upgrade pip
+sudo pip install neovim && sudo pip install neovim jedi
+sudo add-apt-repository ppa:neovim-ppa/stable
+sudo apt-get update
+sudo apt-get install neovim
+
 ln -sf $HOME/dotfiles/.vimrc $HOME/.vimrc
 ln -sf $HOME/dotfiles/.gvimrc $HOME/.gvimrc
 ln -sf $HOME/dotfiles/.zshrc $HOME/.zshrc
