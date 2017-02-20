9370df761ed84e0d9880c30b69b774ca7f4a053a
URANO Ryoya <uryoya.ur@gmail.com>
2017-02-21 01:13:34 +0900

    不要なリンクの削除とわかりづらい名前の変更

--- install.sh
+++ install.sh
@@ -5,8 +5,5 @@ ln -sf $HOME/dotfiles/.gvimrc $HOME/.gvimrc
 ln -sf $HOME/dotfiles/.zshrc $HOME/.zshrc
 ln -sf $HOME/dotfiles/.vim $HOME/.vim
 ln -sf $HOME/dotfiles/nvim $HOME/.config/nvim
-ln -sf $HOME/dotfiles/.Xresources $HOME/.Xresources
-ln -sf $HOME/dotfiles/.zshenv $HOME/.zshenv
 ln -sf $HOME/dotfiles/.tmux.conf $HOME/.tmux.conf
-
 
