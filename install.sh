2bf88945ec5ae5e008d1d3fe198ca5c17caf3dbd
URANO Ryoya <urano.works.mail@gmail.com>
2017-10-21 14:26:11 +0900

    Add -y option when install via apt

--- install.sh
+++ install.sh
@@ -9,15 +9,15 @@ git config --global user.email "urano.works.mail@gmail.com"
 git config --global user.name "URANO Ryoya"
 
 # python
-sudo apt install python3-venv
+sudo apt install python3-venv -y
 
 # for neovim
-sudo apt install rsync xsel python-pip python3-pip
-sudo pip install --upgrade pip && sudo pip3 install --upgrade pip
-sudo pip install neovim && sudo pip3 install neovim jedi
+sudo apt install rsync xsel python-pip python3-pip -y
+sudo pip install --upgrade pip -y && sudo pip3 install --upgrade pip -y
+sudo pip install neovim -y && sudo pip3 install neovim jedi -y
 sudo add-apt-repository ppa:neovim-ppa/stable
 sudo apt-get update
-sudo apt-get install neovim
+sudo apt-get install neovim -y
 
 ln -sf $HOME/dotfiles/.vimrc $HOME/.vimrc
 # ln -sf $HOME/dotfiles/.gvimrc $HOME/.gvimrc
