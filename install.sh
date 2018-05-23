1b6dba2749142722164a48d20b49fe1bc67a5b9c
URANO Ryoya <urano.works.mail@gmail.com>
2018-05-24 02:21:36 +0900

    依存関係の解決順を修正

--- install.sh
+++ install.sh
@@ -4,6 +4,12 @@
 git config --global user.email "urano.works.mail@gmail.com"
 git config --global user.name "URANO Ryoya"
 
+# Symlinkを貼って各設定ファイルを有効化
+ln -sf $HOME/dotfiles/.vimrc $HOME/.vimrc
+ln -sf $HOME/dotfiles/.vim $HOME/.vim
+ln -sf $HOME/dotfiles/nvim $HOME/.config/nvim
+ln -sf $HOME/dotfiles/.tmux.conf $HOME/.tmux.conf
+
 # Neovim用に使うプラグインマネージャ
 curl -fLo $HOME/.config/nvim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
@@ -12,12 +18,6 @@ curl -fLo $HOME/.config/nvim/autoload/plug.vim --create-dirs \
 curl "https://raw.githubusercontent.com/altercation/vim-colors-solarized/master/colors/solarized.vim" \
     --create-dirs -o $HOME/.vim/colors/solarized.vim
 
-# Symlinkを貼って各設定ファイルを有効化
-ln -sf $HOME/dotfiles/.vimrc $HOME/.vimrc
-ln -sf $HOME/dotfiles/.vim $HOME/.vim
-ln -sf $HOME/dotfiles/nvim $HOME/.config/nvim
-ln -sf $HOME/dotfiles/.tmux.conf $HOME/.tmux.conf
-
 # fishを起動するように(デフォルトシェルは変えない)
-echo "exec fish" >> $HOME/.bashrc
+# echo "exec fish" >> $HOME/.bashrc
 
