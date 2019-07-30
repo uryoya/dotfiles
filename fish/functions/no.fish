4023fdacdaaaf69ffd38d29f7aa54cb5e74a3326
URANO Ryoya <urano.works.mail@gmail.com>
2019-07-30 14:19:29 +0900

    remove unused functions

--- fish/functions/no.fish
+++
@@ -1,3 +0,0 @@
-function no --description 'search file by fzf and open in NVim'
-    nvim (find . | fzf --reverse)
-end
--- fish/functions/rustrun.fish
+++
@@ -1,7 +0,0 @@
-function rustrun --description 'rust compile and run'
-    set PROG_NAME '_out'
-    rustc $argv[1] -o $PROG_NAME
-    eval ./$PROG_NAME $argv[2..-1]
-    rm $PROG_NAME
-end
-
