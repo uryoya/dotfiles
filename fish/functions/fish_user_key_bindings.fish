661b020aeaa9ff33fea29cbf3f70b7d91fba9ed9
URANO Ryoya <urano.works.mail@gmail.com>
2020-03-11 11:29:35 +0900

    add keybind that search repository

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -1,3 +1,4 @@
 function fish_user_key_bindings
     bind \cr '__search_history'
+    bind \cg 'repos'
 end
--- fish/functions/repos.fish
+++ fish/functions/repos.fish
@@ -1,5 +1,6 @@
 function repos -d 'Search github repositories by ghq'
     ghq list --full-path | fzf --preview "__preview_repos_readme {}" | read repository
     [ -n "$repository" ] && cd $repository
+    commandline -f repaint # it refresh prompt for keybind (editor command)
 end
 
