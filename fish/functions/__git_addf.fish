bcfcd2ee3c1ab72b3c2a56fe674834f5005a3188
URANO Ryoya <urano.works.mail@gmail.com>
2022-04-05 11:20:27 +0900

    wip

--- fish/functions/__git_addf.fish
+++ fish/functions/__git_addf.fish
@@ -1,5 +1,5 @@
 function __git_addf -d 'git add with fzf'
-    set -l selected (unbuffer git status -s | fzf -m --reverse --border --ansi --preview="echo {} | awk '{print \$2}' | xargs git diff --color" | awk '{print $2}')
+    set -l selected (unbuffer git status -s | fzf -m --reverse --border --ansi --preview="echo {} | awk '{print \$2}' | xargs git diff | delta --syntax-theme 'Dracula'" | awk '{print $2}')
     if [ -n "$selected" ]
         set -l formated_s (echo $selected | tr '\n' ' ' | sed 's/ $//g')
         set -l formated_s (echo $formated_s | string split ' ')
