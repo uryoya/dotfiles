ea9bf2f2daee69a78f22856459cfa9275a56d217
URANO Ryoya <urano.works.mail@gmail.com>
2022-07-07 01:57:19 +0900

    2回以上実行すると不要なファイルが生成されていたので修正

--- install.sh
+++ install.sh
@@ -1,14 +1,22 @@
 #! /bin/bash
 
 # Symlinkを貼って各設定ファイルを有効化
-ln -sf $HOME/dotfiles/.vimrc $HOME/.vimrc
-ln -sf $HOME/dotfiles/.vim $HOME/.vim
-ln -sf $HOME/dotfiles/nvim $HOME/.config/nvim
-ln -sf $HOME/dotfiles/.tmux.conf $HOME/.tmux.conf
-ln -sf $HOME/dotfiles/.gitconfig $HOME/.gitconfig
-ln -sf $HOME/dotfiles/fish/creel.fish $HOME/.config/fish/functions/creel.fish
-ln -sf $HOME/dotfiles/.zshrc $HOME/.zshrc
-ln -sf $HOME/dotfiles/.zprofile $HOME/.zprofile
+[ ! -e $HOME/.vimrc ] \
+    && ln -sf $HOME/dotfiles/.vimrc $HOME/.vimrc
+[ ! -d $HOME/.vim ] \
+    && ln -sf $HOME/dotfiles/.vim $HOME/.vim
+[ ! -d $HOME/.config/nvim ] \
+    && ln -sf $HOME/dotfiles/nvim $HOME/.config/nvim
+[ ! -e $HOME/.tmux.conf ] \
+    && ln -sf $HOME/dotfiles/.tmux.conf $HOME/.tmux.conf
+[ ! -e $HOME/.gitconfig ] \
+    && ln -sf $HOME/dotfiles/.gitconfig $HOME/.gitconfig
+[ ! -e $HOME/.config/fish/functions/creel.fish ] \
+    && ln -sf $HOME/dotfiles/fish/creel.fish $HOME/.config/fish/functions/creel.fish
+[ ! -e $HOME/.zshrc ] \
+    && ln -sf $HOME/dotfiles/.zshrc $HOME/.zshrc
+[ ! -e $HOME/.zprofile ] \
+    && ln -sf $HOME/dotfiles/.zprofile $HOME/.zprofile
 
 # Neovimのプラグインマネージャ
 plug=$HOME/.config/nvim/autoload/plug.vim
